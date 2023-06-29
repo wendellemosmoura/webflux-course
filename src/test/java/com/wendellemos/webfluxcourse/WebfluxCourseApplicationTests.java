d206d9c1e9181b1b8536f26881bce9ee49f21788
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2023-06-29 09:33:48 -0300

    start project

---
+++ src/test/java/com/wendellemos/webfluxcourse/WebfluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendellemos.webfluxcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
