f5e5ca9fa95cb91750eaeb42cef14fd4ad1fd8ff
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2023-06-29 15:08:08 -0300

    added exception handler to field errors

--- src/main/java/com/wendellemos/webfluxcourse/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/wendellemos/webfluxcourse/exceptions/ControllerExceptionHandler.java
@@ -2,14 +2,16 @@ package com.wendellemos.webfluxcourse.exceptions;
 
 
 import org.springframework.dao.DuplicateKeyException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
-import java.time.LocalDateTime;
+import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -18,12 +20,25 @@ public class ControllerExceptionHandler {
     ResponseEntity<Mono<StandardError>> duplicateKeyException(DuplicateKeyException ex, ServerHttpRequest request) {
         return ResponseEntity.badRequest().body(Mono.just(
                 StandardError.builder()
-                        .timestamp(LocalDateTime.now())
+                        .timestamp(now())
                         .path(request.getPath().toString())
-                        .status(HttpStatus.BAD_REQUEST.value())
-                        .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                        .status(BAD_REQUEST.value())
+                        .error(BAD_REQUEST.getReasonPhrase())
                         .message(verifyDupKey(ex.getMessage()))
                         .build()));
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<Mono<ValidationError>> validationError(WebExchangeBindException ex, ServerHttpRequest request) {
+        ValidationError error = new ValidationError(
+                now(), request.getPath().toString(), BAD_REQUEST.value(), "Validation Error", "Attribute validation error"
+        );
+
+        for (FieldError x: ex.getBindingResult().getFieldErrors()) {
+            error.addError(x.getField(), x.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(BAD_REQUEST).body(Mono.just(error));
     }
 
     private String verifyDupKey(String message) {
--- src/main/java/com/wendellemos/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/com/wendellemos/webfluxcourse/model/request/UserRequest.java
@@ -1,8 +1,19 @@
 package com.wendellemos.webfluxcourse.model.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserRequest(
+
+        @NotBlank(message = "Name cannot be null or empty")
+        @Size(min = 3, max = 50, message = "Name must contain between 3 and 50 characters")
         String name,
+        @NotBlank(message = "Email cannot be null or empty")
+        @Email(message = "Insert a valid e-mail")
         String email,
+        @NotBlank(message = "Password cannot be null or empty")
+        @Size(min = 6, max = 20, message = "Password must contain between 6 and 20 characters")
         String password
 ) {
 }
