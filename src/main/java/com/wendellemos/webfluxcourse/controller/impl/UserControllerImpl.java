92118eebda1c2e9a54f2cfe0b5d678e3b39cf53a
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2023-06-30 15:13:53 -0300

    added impl to update user contract

--- src/main/java/com/wendellemos/webfluxcourse/controller/impl/UserControllerImpl.java
+++ src/main/java/com/wendellemos/webfluxcourse/controller/impl/UserControllerImpl.java
@@ -42,7 +42,9 @@ public class UserControllerImpl implements UserController {
 
     @Override
     public ResponseEntity<Mono<UserResponse>> upadate(String id, UserRequest request) {
-        return null;
+        return ResponseEntity.ok().body(
+                userService.update(id, request).map(userMapper::toResponse)
+        );
     }
 
     @Override
